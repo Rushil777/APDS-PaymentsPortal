3765d3d84a82af424617cab03fa6aa84a4a81bbe
mickein lutchmadoo <mickein.lutchmadoo@gmail.com>
2024-10-07 15:38:49 +0200

    added a table in employee dashboard and added code in employee-dashboard.component.ts

--- PaymentsPortalAPDS/src/app/employee-dashboard/employee-dashboard.component.ts
+++ PaymentsPortalAPDS/src/app/employee-dashboard/employee-dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PaymentService } from '../payment.service';
+
 
 @Component({
   selector: 'app-employee-dashboard',
@@ -7,6 +9,26 @@ import { Component } from '@angular/core';
   templateUrl: './employee-dashboard.component.html',
   styleUrl: './employee-dashboard.component.css'
 })
-export class EmployeeDashboardComponent {
+export class DashboardComponent implements OnInit {
+  payments: any[] = [];
 
+  constructor(private paymentService: PaymentService) {}
+
+  ngOnInit(): void {
+    this.paymentService.getPayments().subscribe((data: any[]) => {
+      this.payments = data;
+    });
+  }
+
+  verifyPayment(id: string) {
+    this.paymentService.verifyPayment(id).subscribe(response => {
+      alert('Payment Verified');
+    });
+  }
+
+  declinePayment(id: string) {
+    this.paymentService.declinePayment(id).subscribe(response => {
+      alert('Payment Declined');
+    });
+  }
 }
