de7d4cdb91d0a8eb9ec9b1961d8671bbaece8121
mickein lutchmadoo <mickein.lutchmadoo@gmail.com>
2024-10-08 23:14:56 +0200

    app.component change

--- PaymentsPortalAPDS/src/app/app.component.ts
+++ PaymentsPortalAPDS/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';  // Adjust path as needed
 import { DiaryComponent } from './diary/diary.component';  // Adjust path as needed
 import { RouterModule } from '@angular/router';
 
@@ -9,7 +8,7 @@ import { RouterModule } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'], 
   standalone: true,
-  imports:[HeaderComponent, DiaryComponent, RouterModule]
+  imports:[DiaryComponent, RouterModule]
 })
 
 export class AppComponent {
